3c997f925e83b8ddc497e948e5a8c2cede720bd2
Oskar Jakieła <oskar.jakiela@allegrogroup.com>
2018-03-06 14:40:19 +0100

    Display `REACT_APP_API_URL` in <App />

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+
+
+const { REACT_APP_API_URL } = process.env;
 
 class App extends Component {
   render() {
@@ -10,8 +14,15 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <p className="App-api-url">
+          <code>
+            {REACT_APP_API_URL}
+          </code>
         </p>
       </div>
     );
