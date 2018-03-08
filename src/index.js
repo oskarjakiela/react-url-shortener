aa46f6a45a45de6a8da092fbc0963e4cdc0cb7a9
Oskar Jakieła <oskar.jakiela@allegrogroup.com>
2018-03-08 09:30:38 +0100

    Fix index.css import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
-import './index.css';
+import 'index.css';
 import registerServiceWorker from 'registerServiceWorker';
 import App from 'components/App';
 import configureStore from 'core/configureStore';
